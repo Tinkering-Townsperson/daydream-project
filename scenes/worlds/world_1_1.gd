63d34eef22c53f015c9e71e7e9b3f5c4f4761857
tinkering-townsperson <h2o.Drop2010@gmail.com>
2025-09-27 17:57:02 -0700

    Add time limit

---
+++ scenes/worlds/world_1_1.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+var time_remaining: int = 30
+var time_since_last_update: float = 0.0
+var gameover: bool = false
+
+var y_win: PackedScene = preload("res://scenes/y_win.tscn")
+var d_win: PackedScene = preload("res://scenes/d_win.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if gameover:
+		var screen = y_win.instantiate()
+		$"..".add_child(screen)
+		queue_free()
+		return
+	
+	time_since_last_update += delta
+	
+	if time_since_last_update >= 1:
+		time_remaining -= 1
+		time_since_last_update -= 1
+		$Camera2D/Label.text = str(time_remaining)
+		
+	if time_remaining <= 0:
+		gameover = true
+	
